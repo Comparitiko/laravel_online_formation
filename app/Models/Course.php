24191f5f397c3718b74a7e5e5495cf1a0d7f2402
Comparitiko <gabigcy@gmail.com>
2025-02-04 11:16:40 +0100

    Easy relationships completed

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -4,9 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Course extends Model
 {
     /** @use HasFactory<\Database\Factories\CourseFactory> */
     use HasFactory;
+
+    public function categories(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function profesor(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'profesor_id');
+    }
+
+    public function materials(): HasMany
+    {
+        return $this->hasMany(CourseMaterial::class);
+    }
 }
