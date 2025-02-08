23ee95f2706c2a52c680012430e3145c90d1c3c4
Comparitiko <gabigcy@gmail.com>
2025-02-08 22:39:00 +0100

    Created command for recreate database

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,10 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Command to recreate the database and run seeds.
+Artisan::command('db:recreate', function () {
+    $this->call('migrate:fresh');
+    $this->call('db:seed');
+    $this->info('Database recreated and seeded!');
+});
