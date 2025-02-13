9061e300aed860fc605bc12b6b66e4fbfafd1b09
Comparitiko <gabigcy@gmail.com>
2025-02-13 16:48:24 +0100

    Configured default response to api with not founds routes and configured not found pages to show the 404 error page

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +16,15 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (NotFoundHttpException $e, $request) {
+            // Check if the request is an API request to return a JSON response
+            if ($request->is('api') || $request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Not found',
+                ], 404);
+            }
+
+            // Return a 404 page for all other requests
+            return response()->view('pages.404', [], 404);
+        });
     })->create();
