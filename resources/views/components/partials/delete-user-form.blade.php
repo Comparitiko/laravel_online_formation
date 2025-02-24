7054b223d307843580eae89b789885141b249b74
Comparitiko <gabigcy@gmail.com>
2025-02-24 23:06:44 +0100

    Delete user form completed

--- resources/views/components/partials/delete-user-form.blade.php
+++ resources/views/components/partials/delete-user-form.blade.php
@@ -1,53 +1,56 @@
 <section class="space-y-6">
     <header>
         <h2 class="text-lg font-medium text-gray-900">
-            {{ __('Delete Account') }}
+            Borrar cuenta
         </h2>
 
         <p class="mt-1 text-sm text-gray-600">
-            {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting your account, please download any data or information that you wish to retain.') }}
+            Una vez que tu cuenta haya sido eliminada todos tus datos van a ser eliminados permanentemente
         </p>
     </header>
 
     <x-danger-button
-        x-data=""
+        type="submit"
+        onclick="document.querySelector('#modal').classList.toggle('hidden')"
     >
-        {{ __('Delete Account') }}
+        Borrar cuenta
     </x-danger-button>
+
+    <div id="modal" class="hidden">
         <form method="post" action="{{ route('profile.destroy') }}" class="p-6">
             @csrf
             @method('delete')
 
             <h2 class="text-lg font-medium text-gray-900">
-                {{ __('Are you sure you want to delete your account?') }}
+                ¿Estas seguro de querer eliminar tu cuenta?
             </h2>
 
-            <p class="mt-1 text-sm text-gray-600">
-                {{ __('Once your account is deleted, all of its resources and data will be permanently deleted. Please enter your password to confirm you would like to permanently delete your account.') }}
-            </p>
-
             <div class="mt-6">
-                <x-input-label for="password" value="{{ __('Password') }}" class="sr-only" />
+                <x-input-label for="password" value="Contraseña" class="sr-only" />
 
                 <x-text-input
                     id="password"
                     name="password"
                     type="password"
                     class="mt-1 block w-3/4"
-                    placeholder="{{ __('Password') }}"
+                    placeholder="Contraseña"
                 />
 
                 <x-input-error :messages="$errors->userDeletion->get('password')" class="mt-2" />
             </div>
 
             <div class="mt-6 flex justify-end">
-                <x-secondary-button>
-                    {{ __('Cancel') }}
+                <x-secondary-button
+                    class="border-2 shadow-lg hover:underline"
+                    onclick="document.querySelector('#modal').classList.add('hidden')"
+                >
+                    Cancelar
                 </x-secondary-button>
 
                 <x-danger-button class="ms-3">
-                    {{ __('Delete Account') }}
+                    Borrar cuenta
                 </x-danger-button>
             </div>
         </form>
+    </div>
 </section>
