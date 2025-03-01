d39011ae3445148d3a671f8f46d2d4cf26362b60
Comparitiko <gabigcy@gmail.com>
2025-03-01 16:38:36 +0100

    Finish course route completed

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -10,25 +10,61 @@ use App\Http\Resources\Course\BaseCourseResource;
 use App\Models\Category;
 use App\Models\Course;
 use App\Models\User;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Foundation\Application;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class CourseController extends Controller
 {
+    /**
+     * Handle route to show courses in the private side of the web
+     * @param Request $request
+     * @return Factory|\Illuminate\Contracts\View\View|Application|object
+     */
     public function private_courses(Request $request)
     {
         $user = $request->user();
+
         if ($user->isAdmin()) $courses = Course::paginate(10);
         else $courses = Course::where('teacher_id', $user->id)->paginate(10);
 
         return view('pages.private.courses', ['courses' => $courses]);
     }
 
+    /**
+     * Handle route to finish a course in the private side of the web
+     * @param Course $course
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function private_finish_course(Course $course, Request $request): RedirectResponse
+    {
+        if ($request->user()->cannot('cancelCourse', $course)) abort(404);
+
+        // Update the course state
+        $course->state = CourseState::FINISHED;
+        $course->save();
+
+        return redirect()->route('private.courses.index');
+    }
+
+    /**
+     * Handle the route to show all courses in the public side of the webº
+     * @return View
+     */
     public function public_course_index(): View
     {
         return view('pages.public.courses');
     }
 
+    /**
+     * Handle route to show all courses in API
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
     public function api_index(Request $request)
     {
         $limit = $request->get('limit', 10);
@@ -80,11 +116,21 @@ class CourseController extends Controller
         return BaseCourseResource::collection($courses);
     }
 
+    /**
+     * Handle route to show the information of one course in API
+     * @param Course $course
+     * @return AllInfoCourseResource
+     */
     public function api_show(Course $course)
     {
         return AllInfoCourseResource::make($course);
     }
 
+    /**
+     * Handle route to create a course in API
+     * @param CreateCourseRequest $request
+     * @return JsonResponse
+     */
     public function api_create(CreateCourseRequest $request)
     {
         // Check if the user can create a course
@@ -107,6 +153,12 @@ class CourseController extends Controller
         return response()->json(['message' => 'Course created successfully']);
     }
 
+    /**
+     * Handle route to delete a course in API
+     * @param Request $request
+     * @param Course $course
+     * @return JsonResponse
+     */
     public function api_delete(Request $request, Course $course)
     {
         if ($request->user()->cannot('deleteCourse', Course::class)) {
--- app/Policies/CoursePolicy.php
+++ app/Policies/CoursePolicy.php
@@ -3,6 +3,7 @@
 namespace App\Policies;
 
 use App\Enums\UserRole;
+use App\Models\Course;
 use App\Models\User;
 
 class CoursePolicy
@@ -19,13 +20,38 @@ class CoursePolicy
         return null;
     }
 
+    /**
+     * Check if the user can create a course
+     * @return bool
+     */
     public function createCourse(): bool
     {
         return false;
     }
 
+    /**
+     * Check if user can delete course
+     * @return bool
+     */
     public function deleteCourse(): bool
     {
         return false;
+    }
+
+    /**
+     * Check if the user can cancel the course
+     * @param User $user
+     * @param Course $course
+     * @return bool
+     */
+    public function cancelCourse(User $user, Course $course): bool
+    {
+        // Check if course is active
+        if (!$course->isActive()) return false;
+
+        // Check if the user is the teacher of the course
+        if (!$user->isTeacherOf($course)) return false;
+
+        return true;
     }
 }
