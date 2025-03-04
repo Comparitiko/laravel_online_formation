d42ac3bfeff33e9dbb7a0cadafb3599e5e9ce797
Comparitiko <gabigcy@gmail.com>
2025-03-04 22:25:12 +0100

    Put expiration time on 1 day

--- app/Notifications/VerifyEmail.php
+++ app/Notifications/VerifyEmail.php
@@ -52,7 +52,7 @@ class VerifyEmail extends Notification
     {
         return URL::temporarySignedRoute(
             'verification.verify',
-            now()->addMinutes(15),
+            now()->addDay(),
             ['id' => $notifiable->getKey(), 'hash' => sha1($notifiable->getEmailForVerification())]
         );
     }
